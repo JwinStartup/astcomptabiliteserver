610d585bb801c2dc2ed33303f7c92940770d467e
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-03-29 22:52:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
   {
-  origin:https://astcomptabilite.netlify.app,
+  origin:"https://astcomptabilite.netlify.app",
   credentials:true
   }
 ));
