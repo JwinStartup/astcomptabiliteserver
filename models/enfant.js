92fa81981f2f97008e41a7a3300811cefa6f9c9e
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-03-31 14:18:53 +0000

    Update enfant.js

--- models/enfant.js
+++ models/enfant.js
@@ -1,1 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
+var enfantSchema = new Schema({
+  nom: {
+    type: String,
+  },
+  prenoms: {
+    type: String,
+  },
+  ville: {
+    type: String,
+  },
+  commune: {
+    type: String,
+  },
+  quartier: {
+    type: String,
+  },
+ 
+  classe: {
+    type: String,
+  },
+  formateur:[{
+    type:Schema.Types.ObjectId,
+ ref:'Personnel'
+}],
+  creerPar:{
+    type:Schema.Types.ObjectId,
+ ref:'User'
+},
+  parent:{
+    type:Schema.Types.ObjectId,
+ ref:'Parent'
+}
+},
+{
+  timestamps: true
+});
+
+module.exports = mongoose.model("Enfant", enfantSchema);
