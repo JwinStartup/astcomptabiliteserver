13d656ee0a48e2ea5f160a1e088d60b9ce0dfbcb
unknown <jeanakoupaul@gmail.com>
2025-06-28 10:12:54 +0000

    maj010000

--- Controllers/comptabiliteController.js
+++ Controllers/comptabiliteController.js
@@ -114,7 +114,7 @@ const payerEncoreFacture= async (req, res, next) => {
             }
             //convertir req.body.monTantPayer en valeur numerique
             facture.montantPayer += Number(req.body.montantPayer)
-            facture.resteApayer -= facture.montantPayer
+            facture.resteApayer =Number(facture.montantPayer) - Number(req.body.montantPayer)
             facture.paiement.push(paiement._id)
             if (facture.resteApayer === 0) {
                 facture.type = "totalite"
